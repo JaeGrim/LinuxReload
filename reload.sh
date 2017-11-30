ce80b8dd463977d0f58ef42163e9974c3e1125ab
jae <shaojaechen@gmail.com>
2017-11-30 14:58:17 -0800

    Added Bash file for linux

---
+++ reload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt-get install -y lamp-server^
+sudo apt install -y virtualbox
+sudo apt install -y vim
+sudo apt install -y git
+sudo apt install -y composer
+sudo apt-get install -y nodejs
+
