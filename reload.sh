c090b6828805b11c0208483f373925f3e27eb0f2
jae <shaojaechen@gmail.com>
2017-12-08 14:52:55 -0800

    added apt install for php mbstring, dom, curl

--- reload.sh
+++ reload.sh
@@ -7,5 +7,11 @@ sudo apt install -y virtualbox
 sudo apt install -y vim
 sudo apt install -y git
 sudo apt install -y composer
-sudo apt-get install -y nodejs
+sudo apt-get install -y node.js
+sudo apt install -y npm
+npm install -g npm@4.6.1
+sudo apt install -y php-mbstring
+sudo apt install -y php-dom
+sudo apt install -y php-curl
+ssh-keygen
 
